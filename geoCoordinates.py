76ba9b90f6b60c3b78895bb8371c22f2885e355f
Matthieu Mourisson <mareek@gmail.com>
2019-09-14 20:17:26 +0200

    added functions distance form equator and from greenwich

--- geoCoordinates.py
+++ geoCoordinates.py
@@ -1,3 +1,5 @@
+import math
+
 class location:
     def __init__(self, latitude, longitude, elevation=None):
         self.latitude = latitude
@@ -6,6 +8,13 @@ class location:
 
     def toLine(self, separator):
         return separator.join(map(lambda f: str(f), [self.latitude, self.longitude, self.elevation]))
+
+    def distanceFromEquatorInMeter(self):
+        return self.latitude * 10000000 / 90
+
+    def distanceFromGreenwichMeridianInMeter(self):
+        latitudeFactor = math.cos(math.radians(abs(self.latitude)))
+        return latitudeFactor * self.longitude * 10000000 / 90
 
     @staticmethod
     def headerLine(separator):

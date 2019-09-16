91d174008c12c05eeb86bc470b30fa031c707934
Matthieu Mourisson <mareek@gmail.com>
2019-09-16 23:05:14 +0200

    Added location.ComputeLatitude & ComputeLongitude

--- geoTools/geoCoordinates.py
+++ geoTools/geoCoordinates.py
@@ -1,13 +1,11 @@
 import math
 
+
 class location:
     def __init__(self, latitude, longitude, elevation=None):
         self.latitude = latitude
         self.longitude = longitude
         self.elevation = elevation
-
-    def toLine(self, separator):
-        return separator.join(map(lambda f: str(f), [self.latitude, self.longitude, self.elevation]))
 
     def distanceFromEquatorInMeter(self):
         return self.latitude * 10000000 / 90
@@ -15,6 +13,20 @@ class location:
     def distanceFromGreenwichMeridianInMeter(self):
         latitudeFactor = math.cos(math.radians(abs(self.latitude)))
         return latitudeFactor * self.longitude * 10000000 / 90
+
+    @staticmethod
+    def computeLatitude(distanceFromeEquatorInMeter):
+        return 90 * distanceFromeEquatorInMeter / 10000000
+    
+    @staticmethod
+    def ComputeLongitude(distanceFromGreenwichMeridianInMeter, distanceFromeEquatorInMeter):
+        latitude = location.computeLatitude(distanceFromeEquatorInMeter)
+        latitudeFactor = 1 / math.cos(math.radians(abs(latitude)))
+        return 90 * latitudeFactor * distanceFromGreenwichMeridianInMeter/10000000
+
+
+    def toLine(self, separator):
+        return separator.join(map(lambda f: str(f), [self.latitude, self.longitude, self.elevation]))
 
     @staticmethod
     def headerLine(separator):
--- test/testGeoCoordinates.py
+++ test/testGeoCoordinates.py
@@ -16,6 +16,23 @@ class TestLocation(unittest.TestCase):
     def test_pointOnFortyFiveDegreeSouthIsMinusFiveThousandKilometerFromEquator(self):
         self.assertEqual(location(-45, 0).distanceFromEquatorInMeter(), -5000000)
 
+    def test_ComputeLatitudeOnEquator(self):
+        self.assertEqual(location.computeLatitude(0), 0)
+
+    def test_ComputeLatitudeOnNorthPole(self):
+        self.assertEqual(location.computeLatitude(10000000), 90)
+
+    def test_ComputeLatitudeAtBordeaux(self):
+        self.assertEqual(location.computeLatitude(5000000), 45)
+
+    def test_ComputeLongitudeAtEquatorLevel(self):
+        self.assertEqual(location.ComputeLongitude(10000000, 0), 90)
+
+    def test_ComputeLongitudeOFRomanSurIsere(self):
+        longitude = location.ComputeLongitude(400000, 5000000)
+        self.assertGreater(longitude, 4.9)
+        self.assertLess(longitude, 5.1)
+
 
 if __name__ == '__main__':
     unittest.main()

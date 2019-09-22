862324a47c785f947320244993327843e47b6ae6
Matthieu Mourisson <mareek@gmail.com>
2019-09-22 22:13:46 +0200

    Implemented sqlite storage (not tested)

--- geoTools/locationStorage.py
+++ geoTools/locationStorage.py
@@ -9,13 +9,24 @@ class locationDatabase:
 
     # create table if it does not exists
     def _initDatabase(self):
-        pass
+        cur = self.connection.cursor()
+        cur.execute("create table if not exists location(latitude real not null, longitude real not null, elevation int not null, primary key(latitude, longitude))")
+        cur.execute("create index if not exists idx_location_latitude on location(latitude)")
+        cur.execute("create index if not exists idx_location_longitude on location(longitude)")
+        cur.close()
 
     # Get location from the database
     def getLocation(self, latitude, longitude):
-        pass
+        cur = self.connection.cursor()
+        cur.execute("select elevation from location where latitude = ? and longitude = ?", (latitude, longitude))
+        elevation = cur.fetchone()
+        cur.close()
+        if elevation == None:
+            return None
+        else:
+            return location(latitude, longitude, next(elevation))
 
     # Add the location to the database
     def addLocation(self, location):
-        pass
-
+        row = (location.latitude, location.longitude, location.elevation)
+        self.connection.execute("insert into location (latitude, longitude, elevation) values (?,?,?)", row)
